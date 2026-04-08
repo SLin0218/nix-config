80c89d2c14e22112608049fb137c9d6d52b87795
DengShilin <dengsl.dev@gmail.com>
2026-04-08 16:05:24 +0800

    sync

--- config/emacs/init-package.el
+++ config/emacs/init-package.el
@@ -13,7 +13,7 @@
 
 (setq use-package-always-ensure t)
 
-(add-to-list 'load-path (expand-file-name "site-lisp/awesome-tab" user-emacs-directory))
-(add-to-list 'load-path (expand-file-name "site-lisp/lsp-bridge" user-emacs-directory))
+(add-to-list 'load-path "~/.config/slin-emacs/site-lisp/awesome-tab")
+(add-to-list 'load-path "~/.config/slin-emacs/site-lisp/lsp-bridge")
 
 (provide 'init-package)

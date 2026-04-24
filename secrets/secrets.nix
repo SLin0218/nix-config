cf44f82daae9f7334d74ff6ab10a03a61e1c9a82
DengShilin <dengsl.dev@gmail.com>
2026-04-24 16:41:39 +0800

    sync publickey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,8 @@ let
   lin1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQc4Ee4JCORNkLSER9OiCoAvRwEYKafjEQdfGf0jxpe lin@inspiron-lin";
   root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAYXENc2QFQkZ4+eWD+/40t0bGWBcCGxxaXyVbtaSky3 root@inspiron-lin";
   nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpK4E3961f8GI1tRFLhGz63AMqPYdboF/3BDamAPri0 root@nixos";
+  fcdeMac-mini = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA1G1PXp3bbePi2il3+iV/6L/3yVkPyen6n5DkZrtI4f lin@fcdeMac-mini";
 in
 {
-  "update-subscription.age".publicKeys = [ lin1 root nixos ];
+  "update-subscription.age".publicKeys = [ lin1 root nixos fcdeMac-mini ];
 }

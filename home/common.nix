0f06d378baae3a0eba0493bc4b6ba85148ffa9c5
DengShilin <dengsl.dev@gmail.com>
2026-08-03 11:35:12 +0800

    feat: 从home-manger迁移到chezmoi

--- home/common.nix
+++ home/common.nix
@@ -1,35 +1,41 @@
 {
-  config,
   pkgs,
   lib,
   ...
 }:
-
+let
+  isWSL =
+    builtins.pathExists /proc/sys/kernel/osrelease
+    && (
+      let
+        release = builtins.readFile /proc/sys/kernel/osrelease;
+      in
+      builtins.match ".*[mM][iI][cC][rR][oO][sS][oO][fF][tT].*" release != null
+    );
+in
 {
   imports = [
     # ./modules/zsh.nix
     # ./modules/starship.nix
     # ./modules/kitty.nix
     # ./modules/wezterm.nix
-    ./modules/theme.nix
-    ./modules/fastfetch.nix
+    # ./modules/fastfetch.nix
     # ./modules/editor.nix
+    ./modules/theme.nix
   ];
 
   home.file.".sqlfluff".source = ../config/sqlfluff;
   home.file.".gitconfig".source = ../config/gitconfig;
 
-  # home.file.".gnupg/gpg-agent.conf".text = ''
-  #   default-cache-ttl 600
-  #   max-cache-ttl 7200
-  #   pinentry-program ${config.home.homeDirectory}/.gnupg/pinentry-wrapper.sh
-  #   allow-loopback-pinentry
-  #   enable-ssh-support
-  # '';
-  # home.file.".gnupg/pinentry-wrapper.sh" = {
-  #   source = ../config/gnupg/pinentry-wrapper.sh;
-  #   executable = true;
-  # };
+  xdg.configFile."emacs/init.el".text = ''
+    ;; -*- lexical-binding: t; -*-
+    (setq nix-librime-path "${pkgs.librime}")
+    (setq nix-rime-share-data-path "${pkgs.rime-data}")
+    (setq nix-jbrsdk-path "${pkgs.jbrsdk-17}")
+    (setq nix-openjdk21-path "${pkgs.openjdk21}")
+    (add-to-list 'load-path "~/.config/slin-emacs")
+    (require 'slin-emacs)
+  '';
 
   home = {
     username = "lin";
@@ -57,6 +63,7 @@
       })
       nmap
       bind.dnsutils
+      fastfetch
 
       # rime
       librime
@@ -88,7 +95,13 @@
 
       # gui
       jetbrains.idea
+    ]
+    # gui
+    ++ lib.optionals (!isWSL) [
       wezterm
+    ]
+    ++ lib.optionals (!pkgs.stdenv.isDarwin) [
+      emacs-pgtk
     ]
     ++ lib.optionals pkgs.stdenv.isLinux [
       pinentry-gnome3
@@ -96,8 +109,6 @@
 
   home.sessionVariables = {
     EDITOR = "nvim";
-    SSH_AUTH_SOCK = "$(gpgconf --list-dir agent-ssh-socket)";
-    GPG_TTY = "$(tty)";
     JAVA_HOME = "${pkgs.jbrsdk-17.home or pkgs.jbrsdk-17}";
     JAVA21_HOME = "${pkgs.openjdk21}";
   };
